a28c4d104db07ff2f79af9ddae7c7399d84dc9ee
madsanta <axell33t@gmail.com>
2019-12-07 20:27:54 +0300

    Add files via upload

--- static/init.js
+++ static/init.js
@@ -1,2 +1,2 @@
-webpackJsonp([1],[function(e,n,t){var i=(t(1),t(2),t(4));window.jQuery=i,window.$=i}]);
+webpackJsonp([1],[function(e,t,n){var i=(n(1),n(2),n(4));window.jQuery=i,window.$=i}]);
 //# sourceMappingURL=init.js.map
